a9e07c463f414f8e0ba407bfc61efe0b28b909de
Flying-Bison1018 <danipina2003@gmail.com>
2021-08-28 19:05:03 -0500

    ejercicio 1

---
+++ ejercico 1/src/ejercico/pkg1/Ejercico1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercico.pkg1;
+
+/**
+ *
+ * @author PC
+ */
+public class Ejercico1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         System.out.printf("x=%d\n",2);
+         System.out.printf("el valor de %d+%d es %d\n",2,2,(2+2));
+         
+         System.out.printf("%d-%d\n",(2+3),(3+2));
+        
+        // TODO code application logic here
+    }
+    
+}
